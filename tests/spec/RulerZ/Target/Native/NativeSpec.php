f92acc7962de7fa10fa2f9c800961597f6f6642f
Yassine Guedidi <yassine@guedidi.com>
2023-12-17 21:10:11 +0100

    Remove coduo/phpspec-data-provider-extension

--- tests/spec/RulerZ/Target/Native/NativeSpec.php
+++ tests/spec/RulerZ/Target/Native/NativeSpec.php
@@ -32,12 +32,11 @@ class NativeSpec extends ObjectBehavior
         $this->supports([], CompilationTarget::MODE_SATISFIES)->shouldReturn(true);
     }
 
-    /**
-     * @dataProvider unsupportedTypes
-     */
-    public function it_can_not_filter_other_types($type)
+    public function it_can_not_filter_other_types()
     {
-        $this->supports($type, CompilationTarget::MODE_FILTER)->shouldReturn(false);
+        foreach ($this->unsupportedTypes() as $type) {
+            $this->supports($type, CompilationTarget::MODE_FILTER)->shouldReturn(false);
+        }
     }
 
     public function unsupportedTypes(): array
